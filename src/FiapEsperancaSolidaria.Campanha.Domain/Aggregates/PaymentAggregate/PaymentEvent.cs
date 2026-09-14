a6e41a51b4f4b24bcb077ec8cfb82921ac53e926
Jediel <jediel.dev@gmail.com>
2026-09-14 20:56:47 -0300

    Adicionado a entidade PaymentEvent

---
+++ src/FiapEsperancaSolidaria.Campanha.Domain/Aggregates/PaymentAggregate/PaymentEvent.cs
@@ -0,0 +1,34 @@
+﻿using FiapEsperancaSolidaria.Campanha.Domain.Abstractions;
+
+namespace FiapEsperancaSolidaria.Campanha.Domain.Aggregates.PaymentAggregate;
+
+public class PaymentEvent : IAggregateRoot
+{
+    public Guid PaymentEventId { get; private set; }
+
+    public Guid DonationId { get; private set; }
+
+    public PaymentEventType PaymentEventType { get; private set; }
+
+    public string Observation { get; private set; } = string.Empty;
+
+    public DateTime CreateAt { get; private set; }
+
+    public PaymentEvent(
+        Guid paymentEventId,
+        Guid donationId,
+        PaymentEventType paymentEventType,
+        string observation,
+        DateTime createAt)
+    {
+        PaymentEventId = paymentEventId;
+        DonationId = donationId;
+        PaymentEventType = paymentEventType;
+        Observation = observation;
+        CreateAt = createAt;
+    }
+
+    private PaymentEvent()
+    {
+    }
+}
--- src/FiapEsperancaSolidaria.Campanha.Infrastructure/Migrations/20260914235558_Add-Table-PaymentEvent.Designer.cs
+++ src/FiapEsperancaSolidaria.Campanha.Infrastructure/Migrations/20260914235558_Add-Table-PaymentEvent.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FiapEsperancaSolidaria.Campanha.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FiapEsperancaSolidaria.Campanha.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260914235558_Add-Table-PaymentEvent")]
+    partial class AddTablePaymentEvent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
